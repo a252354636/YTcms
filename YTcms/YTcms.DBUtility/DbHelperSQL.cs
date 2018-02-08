8ae4ee269e4086c5f6d3ef2fbf005c6e033f510a
wangyuting <wangyuting>
2018-02-08 18:24:29 +0800

    第二次提交

---
+++ YTcms/YTcms.DBUtility/DbHelperSQL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data.Common;
+using System.Collections.Generic;
+
+namespace YTcms.DBUtility
+{
+  public abstract class DbHelperSQL
+  {
+    //数据库连接字符串(web.config来配置)，可以动态更改connectionString支持多数据库.		
+    public static readonly string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+    private static readonly string connectionName = ConfigurationManager.AppSettings["ConnectionName"];
+    public static IDbConnection CreateConnection()
+    {
+      IDbConnection conn = null;
+      switch (connectionName)
+      {
+        case "SQLServer":
+          conn = new SqlConnection(connectionString);
+          break;
+        default:
+          conn = new SqlConnection(connectionString);
+          break;
+      }
+      conn.Open();
+      return conn;
+    }
+  }
+}
